011782bf7032b4bea4284aca24ad4faad1a6c608
Vladimir Burkov <vladimir.burkov@egamings.com>
2021-02-25 18:03:39 +0300

    combine gulp tasks and npm

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var rigger = require('gulp-rigger');
 var rename = require("gulp-rename");
 
-gulp.task('build-html', function() {
+gulp.task('build-html', async function() {
     gulp.src('build.html') // Get source files with gulp.src
     .pipe(rigger()) // Sends it through a gulp plugin
     .pipe(rename('index.html'))
